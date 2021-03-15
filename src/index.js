f4d8a3baab94c97945e779bad312ea51dd98a02f
Uladzimir Prybylouski <eibiologos@gmail.com>
2021-03-15 20:59:13 +0300

    fix: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
- 
-}
+    let arrayToStr = str.split("");
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let firstConfig = bracketsConfig[i][0],
+            secondConfig = bracketsConfig[i][1];
+
+        for (let m = 0; m < str.length; m++) {
+            if (
+                arrayToStr[m] == firstConfig &&
+                arrayToStr[m + 1] == secondConfig
+            ) {
+                arrayToStr.splice(m, 2);
+                i = -1;
+                m = -1;
+            }
+        }
+    }
+    if (arrayToStr == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
